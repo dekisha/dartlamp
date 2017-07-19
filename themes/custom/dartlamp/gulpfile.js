d57492fc9ecb5d20e13290e428d25f69ebed0d89
dekisha <dejan@coworks.be>
2017-07-19 10:56:53 +0200

    Rethinking theme structure.

--- themes/custom/dartlamp/gulpfile.js
+++ themes/custom/dartlamp/gulpfile.js
@@ -4,15 +4,16 @@ var gulp = require('gulp'),
   gp_sourcemaps = require('gulp-sourcemaps'),
   gp_postcss = require('gulp-postcss'),
   autoprefixer = require('autoprefixer'),
-  postcss_font_awesome = require('postcss-font-awesome'),
   browserSync = require('browser-sync').create(),
   gp_concat = require('gulp-concat'),
   gp_uglify = require('gulp-uglify'),
   gp_postcss_size = require('postcss-size'),
-  gp_assets  = require('postcss-assets'),
+  gp_assets = require('postcss-assets'),
   gp_filter = require('gulp-filter'),
   gp_livereload = require('gulp-livereload'),
   gp_rucksack = require('gulp-rucksack'),
+  gp_header = require('gulp-header'),
+  postcss_svg = require('postcss-svg'),
   processors = [
     autoprefixer({
       browsers: ['last 5 versions', 'IE 9', 'IE 10'],
@@ -21,8 +22,10 @@ var gulp = require('gulp'),
     gp_assets({
       loadPaths: ['images']
     }),
-    postcss_font_awesome,
     gp_postcss_size,
+    postcss_svg({
+      paths: ['images/svg']
+    })
   ],
   jsfiles = [
     'node_modules/slick-carousel/slick/slick.js',
@@ -30,74 +33,75 @@ var gulp = require('gulp'),
     'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js',
     'node_modules/threesixty-slider/src/threesixty.js',
     'node_modules/jquery-sticky/jquery.sticky.js',
-    'assets/js/scripts.js'
+    'js/src/scripts.js'
   ],
   scssfiles = [
-    'assets/scss/*.scss'
+    'css/src/*.scss'
   ],
   fonts = [
-    'node_modules/font-awesome/fonts/*'
   ];
 
 //SETTINGS
 // Added task for browser-sync, enable browserSync.reload  for livereload
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   browserSync.init({
-    proxy: 'cofe.loc',   // site url
-    open: false          // turnoff auto open on browsersync start
+    proxy: 'cofe.loc', // site url
+    open: false // turnoff auto open on browsersync start
   });
 });
 
 //FA FONTS
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
   return gulp.src(fonts)
-  .pipe(gulp.dest('fonts'));
+    .pipe(gulp.dest('fonts'));
 });
 
 //CSS
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return gulp.src(scssfiles)
+    .pipe(gp_header('$debug: true;'))
     .pipe(gp_sourcemaps.init())
     .pipe(gp_sass().on('error', gp_sass.logError))
     .pipe(gp_rucksack())
     .pipe(gp_postcss(processors))
     .pipe(gp_sourcemaps.write())
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('css/dist'))
     .pipe(gp_filter('**/*.css'))
     .pipe(gp_livereload());
-    // .pipe(browserSync.reload({stream: true}));
+  // .pipe(browserSync.reload({stream: true}));
 });
 
 //CSS MINIFY
-gulp.task('styles_prod', function() {
+gulp.task('styles_prod', function () {
   return gulp.src(scssfiles)
-    .pipe(gp_sass({outputStyle: 'compressed'}).on('error', gp_sass.logError))
+    .pipe(gp_sass({
+      outputStyle: 'compressed'
+    }).on('error', gp_sass.logError))
     .pipe(gp_rucksack())
     .pipe(gp_postcss(processors))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css/dist'));
 });
 
 //JS
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   return gulp.src(jsfiles)
     .pipe(gp_sourcemaps.init())
     .pipe(gp_concat('scripts.js'))
     .pipe(gp_sourcemaps.write('./'))
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('js/dist'));
 });
 
 //JS MINIFY/UGLIFY
-gulp.task('scripts_prod', function() {
+gulp.task('scripts_prod', function () {
   return gulp.src(jsfiles)
     .pipe(gp_concat('scripts.js'))
     .pipe(gp_uglify())
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('js/dist'));
 });
 
 //WATCH
 // gulp.task('watch' ,['browser-sync'] , function() {
-
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(scssfiles, ['styles']);
   gulp.watch(jsfiles, ['scripts']);
   gp_livereload.listen();
